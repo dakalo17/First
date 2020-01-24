07217e744a08ce8ec814ca59caeee38d82628cf3
Gumani <dakalo.luvhengo17@gmail.com>
2020-01-24 13:11:08 +0200

    1st time adding c# project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] num = new int[10];
+            for (int i = 0; i < 9; i++)
+                num[i] = (i + 1);
+            num[9] = 0;
+
+            /*for(int i=0;i<10;i++)
+                Console.WriteLine(num[i]+ "\n");*/
+
+            Console.WriteLine("Output :" + CreatePhoneNumber(num));
+        }
+
+
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            string strNum = "(";
+
+            for (int i = 0; i < numbers.Length; i++)
+                strNum += numbers[i];
+            
+            strNum = strNum.Insert(4, ")");
+            strNum = strNum.Insert(5, " ");
+            strNum = strNum.Insert(9, "-");
+
+            return strNum;
+        }
+
+    }
+}
